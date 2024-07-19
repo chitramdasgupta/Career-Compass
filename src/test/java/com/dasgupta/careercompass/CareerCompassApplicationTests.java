0efafe888266efac38ba93b74942644b0d3c93eb
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-07-19 22:31:59 +0530

    Bootstrapped the Spring Boot backend with DB support

---
+++ src/test/java/com/dasgupta/careercompass/CareerCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasgupta.careercompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
