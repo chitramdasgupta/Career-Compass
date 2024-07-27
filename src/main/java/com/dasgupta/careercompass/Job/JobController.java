e43106616da647d7bc318b51fbb0202c8659b556
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-07-27 15:47:48 +0530

    Added the presentation layer for jobs

---
+++ src/main/java/com/dasgupta/careercompass/Job/JobController.java
@@ -0,0 +1,34 @@
+package com.dasgupta.careercompass.Job;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("jobs")
+public class JobController {
+    private final JobService jobService;
+
+    @Autowired
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/")
+    public List<Job> getAllJobs() {
+        return jobService.getAllJobs();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable int id) {
+        Optional<Job> job = jobService.getJobById(id);
+
+        return job.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
