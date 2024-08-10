215a38261e3369eb655b7bd4dc2e8dd4d1ac1834
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-08-10 16:50:26 +0530

    Added custom styling to the jobs components

--- client/app/jobs/JobDescription.tsx
+++ client/app/jobs/JobDescription.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { Job } from "@/app/jobs/types";
 import { SkeletonList } from "../posts/components/PostSkeleton/skeletonList";
+import Button from "@mui/material/Button/";
+import LaunchIcon from "@mui/icons-material/Launch";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Paper,
+} from "@mui/material";
+import Link from "next/link";
+import { BookmarkAdd, BookmarkAddOutlined } from "@mui/icons-material";
 
 interface JobDescriptionProps {
   job: Job | null;
@@ -11,5 +22,31 @@ export const JobDescription: React.FC<JobDescriptionProps> = ({ job }) => {
     return <SkeletonList count={1} />;
   }
 
-  return <div className="sticky top-2">{job.description}</div>;
+  return (
+    <Card className="sticky top-2">
+      <CardHeader
+        title={job.title}
+        subheader={
+          <Link href={`companies/${job.company.id}`}>{job.company.name}</Link>
+        }
+        sx={{ paddingBottom: "8px" }}
+      />
+      <CardActions sx={{ padding: "0 16px 16px 16px" }}>
+        <Button size="small" variant="contained" endIcon={<LaunchIcon />}>
+          Apply Now
+        </Button>
+        <Button
+          size="small"
+          variant="contained"
+          color="secondary"
+          endIcon={<BookmarkAddOutlined />}
+        >
+          Bookmark
+        </Button>
+      </CardActions>
+      <CardContent>
+        <p>{job.description}</p>
+      </CardContent>
+    </Card>
+  );
 };
--- client/app/jobs/jobItem.tsx
+++ client/app/jobs/jobItem.tsx
@@ -9,7 +9,15 @@ interface JobItemProps {
 
 export const JobItem: React.FC<JobItemProps> = ({ job, onSelect }) => {
   return (
-    <Card className="mb-4 hover:cursor-pointer" onClick={() => onSelect(job)}>
+    <Card
+      className="mb-4 hover:cursor-pointer"
+      onClick={() => onSelect(job)}
+      sx={{
+        "&:hover .MuiCardHeader-title": {
+          textDecoration: "underline",
+        },
+      }}
+    >
       <CardHeader title={job.title} subheader={job.company.name} />
       <CardContent>
         <p>{job.description}</p>
