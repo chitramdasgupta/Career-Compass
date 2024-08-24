41fcdf0e2dbbf52e453b72b785870478d1e8b58d
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-08-24 17:35:56 +0530

    Added support for questions in the persistence layer

---
+++ src/main/java/com/dasgupta/careercompass/questionnaire/Question.java
@@ -0,0 +1,23 @@
+package com.dasgupta.careercompass.questionnaire;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    @Enumerated(EnumType.STRING)
+    private QuestionType type;
+}
---
+++ src/main/java/com/dasgupta/careercompass/questionnaire/QuestionType.java
@@ -0,0 +1,10 @@
+package com.dasgupta.careercompass.questionnaire;
+
+public enum QuestionType {
+    TEXT("TEXT"),
+    SINGLE_SELECT("SINGLE_SELECT"),
+    MULTI_SELECT("MULTI_SELECT");
+
+    QuestionType(String value) {
+    }
+}
