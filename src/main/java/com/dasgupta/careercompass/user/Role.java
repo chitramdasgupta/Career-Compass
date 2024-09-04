d296d0d48f0b663c62fb1be68d51e73f3648956a
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-09-04 21:39:00 +0530

    Added role support

---
+++ src/main/java/com/dasgupta/careercompass/user/Role.java
@@ -0,0 +1,10 @@
+package com.dasgupta.careercompass.user;
+
+public enum Role {
+    ROLE_CANDIDATE("ROLE_CANDIDATE"),
+    ROLE_COMPANY("ROLE_COMPANY"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    Role(String value) {
+    }
+}
